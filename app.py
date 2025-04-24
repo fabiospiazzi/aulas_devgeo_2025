d782ec018e39d9c5b6b181ff5a6ffddce9a11715
fabiospiazzi <fabiospiazzi@gmail.com>
2025-04-24 11:16:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+
+PAGE_CONFIG = {"page_title":"Aplicação de Mapas","page_icon":":smiley:","layout":"centered"}
+st.set_page_config(**PAGE_CONFIG)
+
+def main():
+	st.title("Como adicionar mapas no StreamLit")
+	st.subheader("Baseado num caderno do Colab")
+	menu = ["Home","Mapa"]
+	choice = st.sidebar.selectbox('Menu',menu)
+	if choice == 'Home':
+		st.subheader("Página Inicial 1")
+	elif choice == 'Mapa':
+		st.subheader("Visualizar Mapa")
+		with st.echo():
+			m = folium.Map (location = [-25.5,-49.3], zoom_start =  9)
+			folium_static(m)
+	else: 
+		st.subheader("")
+if __name__ == '__main__':
+	main()
