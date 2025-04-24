10edee20db1b10365ea97096f49cdc864f56dc44
fabiospiazzi <fabiospiazzi@gmail.com>
2025-04-24 18:02:10 -0300

    Update app_distritos_floripa_2.py

--- app_distritos_floripa_2.py
+++ app_distritos_floripa_2.py
@@ -8,10 +8,10 @@ import plotly.express as px
 
 
 
-bairros = 'https:///raw.githubusercontent.com/fabiospiazzi/aulas_devgeo_2025/main/distritos_administrativosWGS84.geojson'
+url_bairros = 'https:///raw.githubusercontent.com/fabiospiazzi/aulas_devgeo_2025/main/distritos_administrativosWGS84.geojson'
 #style = {'fillcolor' : '#F5DEB3' , 'color' : '#8B0000'}
 
-polygons = gpd.read_file(bairros)
+polygons = gpd.read_file(url_bairros)
 
 #m = folium.Map (location = [-27.594605,-48.508875],
  #              tiles = 'openstreetmap',
@@ -23,8 +23,8 @@ polygons = gpd.read_file(bairros)
   # tooltip=folium.GeoJsonTooltip(fields=['nome', 'codigo_ibg']),
   # style_function=lambda x: style).add_to(m)
 
-pontos_onibus = 'https:///raw.githubusercontent.com/fabiospiazzi/aulas_devgeo_2025/main/pontos_onibus_2024_WGS84_TODOS.geojson'
-points = gpd.read_file(pontos_onibus)
+url_pontos_onibus = 'https:///raw.githubusercontent.com/fabiospiazzi/aulas_devgeo_2025/main/pontos_onibus_2024_WGS84_TODOS.geojson'
+points = gpd.read_file(url_pontos_onibus)
 
 # Configuração da página
 PAGE_CONFIG = {"page_title":"Aplicação de Mapas com Pandas", "page_icon":":smiley:", "layout":"centered"}
