04c73b11e2d6b9ee364ac2655218e8ed5f7d9a53
fabiospiazzi <fabiospiazzi@gmail.com>
2025-04-25 15:15:42 -0300

    Update app_distritos_floripa_2.py

--- app_distritos_floripa_2.py
+++ app_distritos_floripa_2.py
@@ -11,38 +11,24 @@ from folium.plugins import MarkerCluster
 from folium.plugins import HeatMap
 
 
-
 url_bairros = 'https://raw.githubusercontent.com/fabiospiazzi/aulas_devgeo_2025/main/distritos_administrativosWGS84.geojson'
-#style = {'fillcolor' : '#F5DEB3' , 'color' : '#8B0000'}
 
 polygons = gpd.read_file(url_bairros)
-
-#m = folium.Map (location = [-27.594605,-48.508875],
- #              tiles = 'openstreetmap',
- #              zoom_start =  12)
-
-#folium.GeoJson(
- #  bairros,
-  # name='bairros',
-  # tooltip=folium.GeoJsonTooltip(fields=['nome', 'codigo_ibg']),
-  # style_function=lambda x: style).add_to(m)
 
 url_pontos_onibus = 'https://raw.githubusercontent.com/fabiospiazzi/aulas_devgeo_2025/main/pontos_onibus_2024_WGS84_TODOS.geojson'
 points = gpd.read_file(url_pontos_onibus)
 
-# Configuração da página
+#Configuração da página
 PAGE_CONFIG = {"page_title":"Aplicação de Mapas com Pandas", "page_icon":":smiley:", "layout":"centered"}
 st.set_page_config(**PAGE_CONFIG)
 
-
-
 def main():
-    # Cria uma dropdown para escolher a regional
+    # Cria um dropdown para escolher a regional
     #polygons['nome'] = polygons['nome'].apply(lambda x: x.encode('latin1').decode('utf-8') if isinstance(x, str) else x)
     regionais = polygons['nome'].unique()
     regional_selecionada = st.sidebar.selectbox('Escolha a regional', regionais)
 
-    # Filtra os bairros por regional selecionada
+    # Filtra os bairros por região ou distrito selecionada
     bairros_filtrados = polygons[polygons['nome'] == regional_selecionada]
 
     # Conta pontos dentro de cada polígono filtrado
@@ -54,7 +40,7 @@ def main():
        
     bairros_filtrados['num_pto'] = pts_in_polys
     
- #Para meu controle, não sei pq o min e o max estão vindo com o mesmo valor, ai coloquei o -1 no min pra não dar erro
+ #Para meu controle, não sei pq o min e o max estão vindo com o mesmo valor. O sliderbar não funcinou.
     print(pts_in_polys)
     print(points.crs)
     print(polygons.crs)
@@ -63,18 +49,18 @@ def main():
     print("pts_in_polys: ",bairros_filtrados['num_pto']) 
  
     #Slidebar para filtrar pelo número de pontos
-#    num_pontos = st.sidebar.slider(
+ #    num_pontos = st.sidebar.slider(
  #   "Número de pontos de ônibus",
  #   int(bairros_filtrados['num_pto'].min()),
  #   int(bairros_filtrados['num_pto'].max()),
  #   (int(bairros_filtrados['num_pto'].min())-(int(bairros_filtrados['num_pto'].max()), int(bairros_filtrados['num_pto'].max())))
-#)
+ #)
 
     
     # Filtra os bairros pelo número de estacionamentos
-#    bairros_finais = bairros_filtrados[bairros_filtrados['num_pto'].between(num_pontos[0], num_pontos[1])]
+ #   bairros_finais = bairros_filtrados[bairros_filtrados['num_pto'].between(num_pontos[0], num_pontos[1])]
 
-    # Plota o histograma
+ # Plota o histograma
  #   f = px.histogram(bairros_finais, x="num_pto", title="Distribuição dos Pontos de Ônibus")
  #   f.update_xaxes(title="Pontos de ônibus")
  #   f.update_yaxes(title="Número")
@@ -82,9 +68,12 @@ def main():
 
     # Mapa com Folium
    # m = folium.Map(location=[-27.594605,-48.508875], zoom_start=13)
- # Calcula o centroide do bairro selecionado
+ 
+ 
+ 
+ # Calcula o centroide do bairro selecionado para dar zoom direto no bairro
     centroide = bairros_filtrados.geometry.centroid.iloc[0]
-    m = folium.Map(location=[centroide.y, centroide.x], zoom_start=14)
+    m = folium.Map(location=[centroide.y, centroide.x], zoom_start=12)
     folium.Choropleth(
         geo_data=bairros_filtrados.to_json(),
         name='Pontos de ônibus por bairro',
@@ -94,19 +83,28 @@ def main():
         fill_color='YlGn',
         legend_name='Pontos de ônibus por bairro ou distrito'
     ).add_to(m)
- 
+
+  #Mostra os pontos de ônbius em todo o município
     #locations = []
     #for idx, row in points.iterrows():
      #locations.append([row['geometry'].y, row['geometry'].x])
-    locations = []
     
+     #Adiciona marcador no centro do bairro
+     folium.Marker(
+         location=[centroide.y, centroide.x],
+         popup=f"Bairro: {regional_selecionada}",
+         icon=folium.Icon(color='blue', icon='info-sign')
+     ).add_to(m) 
+
+    #Mostra os pontos de ônibus somente no bairro selecionado
+    locations = []
     for i, poly in bairros_filtrados.iterrows():
         pts_in_this_poly = points[points.within(poly.geometry)]
         for _, row in pts_in_this_poly.iterrows():
           locations.append([row.geometry.y, row.geometry.x])
 
-
-
+    #Adiciona os pontos selecionados no mapa em formato e cluster
+    
     folium.plugins.MarkerCluster(locations=locations, name='Pontos de Ônibus').add_to(m)
  
     #m.add_children(MarkerCluster(locations=locations, name = 'Pontos de Ônibus de Florianópolis'))
@@ -114,7 +112,7 @@ def main():
     #for idx, row in points.iterrows():
      #locations.append([row['geometry'].y, row['geometry'].x])
 
-    #HeatMap(locations,name = 'Mapa de Calor').add_to(m)
+    HeatMap(locations,name = 'Mapa de Calor').add_to(m)
     
     folium.LayerControl().add_to(m)
     folium_static(m)
