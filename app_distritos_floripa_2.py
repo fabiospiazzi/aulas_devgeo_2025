20323a4cdf453ecd1e9e5ca8207340d048687c4f
fabiospiazzi <fabiospiazzi@gmail.com>
2025-04-25 08:09:57 -0300

    Update app_distritos_floripa_2.py

--- app_distritos_floripa_2.py
+++ app_distritos_floripa_2.py
@@ -50,7 +50,7 @@ def main():
         pts_in_this_poly = points[points.within(poly.geometry)]
         pts_in_polys.append(len(pts_in_this_poly))
 
-   locations2 = []
+    locations2 = []
    for idx, row in points.iterrows():
      locations2.append([row['geometry'].y, row['geometry'].x])
 
