468c797870134c34f31b1f5e107920ec99dc0b49
fabiospiazzi <fabiospiazzi@gmail.com>
2025-04-25 08:34:05 -0300

    Update app_distritos_floripa_2.py

--- app_distritos_floripa_2.py
+++ app_distritos_floripa_2.py
@@ -65,9 +65,9 @@ def main():
     #Slidebar para filtrar pelo número de pontos
     num_pontos = st.sidebar.slider(
     "Número de pontos de ônibus",
-    int(bairros_filtrados['num_pto'].min()-1),
+    int(bairros_filtrados['num_pto'].min()-int(bairros_filtrados['num_pto'].max()),
     int(bairros_filtrados['num_pto'].max()),
-    (int(bairros_filtrados['num_pto'].min()-1), int(bairros_filtrados['num_pto'].max()))
+    (int(bairros_filtrados['num_pto'].min()-int(bairros_filtrados['num_pto'].max()), int(bairros_filtrados['num_pto'].max()))
 )
 
     
